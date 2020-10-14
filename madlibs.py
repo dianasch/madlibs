252db693ae9f4e578d002dae72c98ba32532d1e8
vagrant <vagrant@vagrant.vm>
2020-10-14 20:35:19 +0000

    Edits show_madlib() function to include adjectives.
    Co-authored-by: kayteecannon <kaytee.cannon@me.com>

--- madlibs.py
+++ madlibs.py
@@ -59,12 +59,12 @@ def show_madlib():
     person = request.args.get("person")
     color = request.args.get("color")
     noun = request.args.get("noun")
-    adjective = request.args.get("adjective")
+    adjectives = request.args.getlist("adjective")
 
     return render_template("madlib.html", person=person, 
                                           color=color, 
                                           noun=noun,
-                                          adjective=adjective)
+                                          adjectives=adjectives)
 
 
 
